fd31da72790aba938f39e4806a4108d1389940d2
fhub-1 <flutterhub1.21.0@gmail.com>
2023-04-11 14:05:14 +0300

    working on dark mode switcher

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,63 +1,90 @@
 <template>
   <div>
-    <header class="shadow-md bg-white dark:bg-gray-900">
+
+    <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    </head>
+    <header class="shadow-md dark:bg-gray-900">
+
       <nav class="container mx-auto p-4 flex justify-between items-center">
-        <NuxtLink to="/" class="font-bold">
-          <img src="/logo.png" class="w-60 h-30" />
+        <NuxtLink to="/" class="font-bold flex items-center">
+          <img v-bind:src="logo" class="mr-2" width="240" height="240" />
         </NuxtLink>
-        <div class="flex md:hidden">
-          <button
-            @click="showMenu = !showMenu"
-            class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-100"
-          >
+        <div class="flex items-center md:hidden">
+          <button @click="showMenu = !showMenu"
+            class="text-gray-100 hover:text-gray-200 focus:outline-none focus:text-gray-100">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path
-                v-if="showMenu"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M2.92 5.38l4.95-4.95a1 1 0 0 1 1.41 0l4.95 4.95a1 1 0 0 1-1.41 1.41L8.83 4.83l-4.24 4.24a1 1 0 1 1-1.41-1.41zm18.93 13.23l-4.95 4.95a1 1 0 0 1-1.41 0l-4.95-4.95a1 1 0 1 1 1.41-1.41l4.24 4.24 4.24-4.24a1 1 0 1 1 1.41 1.41z"
-              />
-              <path
-                v-else
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 4H4v2h16v-2z"
-              />
+              <path v-if="!showMenu" fill-rule="evenodd" clip-rule="evenodd"
+                d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 4H4v2h16v-2z" />
+              <path v-else fill-rule="evenodd" clip-rule="evenodd"
+                d="M3.293 4.293a1 1 0 0 1 1.414 0L12 10.586l7.293-7.293a1 1 0 1 1 1.414 1.414L13.414 12l7.293 7.293a1 1 0 1 1-1.414 1.414L12 13.414l-7.293 7.293a1 1 0 0 1-1.414-1.414L10.586 12 3.293 4.707a1 1 0 0 1 0-1.414z" />
             </svg>
           </button>
+
+
         </div>
         <div class="hidden md:flex">
           <ul class="flex gap-4">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-            <!-- <li><NuxtLink to="/project">Project</NuxtLink></li> -->
+            <li class="fas fa-home">
+              <NuxtLink to="/" :class="{ 'router-link-exact-active': $route.path === '/' }">Home</NuxtLink>
+            </li>
+            <li class="fas fa-info-circle">
+              <NuxtLink to="/about" :class="{ 'router-link-exact-active': $route.path === '/about' }">About</NuxtLink>
+            </li>
+            <li class="fas fa-blog">
+              <NuxtLink to="/blog" :class="{ 'router-link-exact-active': $route.path === '/blog' }">Blog</NuxtLink>
+            </li>
           </ul>
         </div>
-        <div class="md:hidden" :class="{ block: showMenu, hidden: !showMenu }">
+        <div class="md:hidden" :class="{ 'block': showMenu, 'hidden': !showMenu }">
           <ul class="mt-2">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/blog">Blog</NuxtLink></li>
-            <!-- <li><NuxtLink to="/project">Project</NuxtLink></li> -->
+            <li>
+              <NuxtLink to="/" :class="{ 'router-link-exact-active': $route.path === '/' }">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about" :class="{ 'router-link-exact-active': $route.path === '/about' }">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/blog" :class="{ 'router-link-exact-active': $route.path === '/blog' }">Blog</NuxtLink>
+            </li>
           </ul>
         </div>
       </nav>
     </header>
-    <!-- adding slog for the content of the page -->
     <div class="container mx-auto p-4">
       <slot />
     </div>
     <Footer />
+    <button @click="toggleDarkMode" class="bg-gray-300 dark:bg-gray-600 rounded-full p-2">
+      <span class="text-gray-800 dark:text-gray-300">
+        {{ darkMode ? 'Light' : 'Dark' }}
+      </span>
+    </button>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
       showMenu: false,
+      logo: "/logo-light.svg",
+      logoWidth: 60,
+      logoHeight: 30,
+      darkMode: false
     };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+      document.documentElement.classList.toggle('dark');
+    },
+  },
+  computed: {
+    logoSrc() {
+      return this.$vuetify.theme.dark ? '/logo-dark.png' : '/logo-light.svg'
+    },
   },
 };
 </script>
@@ -65,5 +92,15 @@ export default {
 <style scoped>
 .router-link-exact-active {
   color: hsl(53, 96%, 49%);
+}
+
+.dark-mode {
+  --bg-color: #1f2937;
+  --text-color: #f3f4f6;
+}
+
+body.dark-mode {
+  background-color: var(--bg-color);
+  color: var(--text-color);
 }
 </style>
--- pages/about.vue
+++ pages/about.vue
@@ -2,41 +2,31 @@
   <section class="py-10 sm:py-16 lg:py-24">
     <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
       <div class="mx-auto text-left md:max-w-lg lg:max-w-2xl md:text-center">
-        <h2
-          class="text-3xl font-bold leading-tight text-slate-100 sm:text-4xl lg:text-5xl lg:leading-tight"
-        >
+        <h2 class="text-3xl font-bold leading-tight text-slate-100 sm:text-4xl lg:text-5xl lg:leading-tight">
           I am building a product that can
           <span class="relative inline-block">
-            <span
-              class="absolute inline-block w-full sm:h-1 bg-yellow-300 bottom-1.5"
-            ></span>
+            <span class="absolute inline-block w-full sm:h-1 bg-yellow-300 bottom-1.5"></span>
             <span class="relative"> solve Bussines’s pain </span>
           </span>
         </h2>
       </div>
 
-      <div
-        class="grid grid-cols-1 mt-8 md:mt-20 gap-y-6 md:grid-cols-2 gap-x-10"
-      >
+      <div class="grid grid-cols-1 mt-8 md:mt-20 gap-y-6 md:grid-cols-2 gap-x-10">
         <div>
-          <img
-            class="w-full mx-auto sm:max-w-xs rounded-md"
-            src="/banner.png"
-            alt="banner"
-          />
+          <img class="w-auto mx-auto sm:max-w-xs rounded-md" src="/profile.JPG" alt="banner" />
           <h1 class="m-10 p-10 text-2xl">some social links</h1>
         </div>
 
         <div>
-          <h3 class="text-lg font-semibold text-gray-900">
+          <h3 class="text-lg font-semibold ">
             Hey! I am John, founder of Celebration.
           </h3>
-          <p class="mt-4 text-lg text-gray-700">
+          <p class="mt-4 text-lg ">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit. Exercitation
             veniam consequat sunt nostrud amet.
           </p>
-          <p class="mt-4 text-lg text-gray-700">
+          <p class="mt-4 text-lg ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -46,7 +36,7 @@
           <h3 class="mt-8 text-lg font-semibold text-gray-900">
             How do I do this without any investment?
           </h3>
-          <p class="mt-4 text-lg text-gray-700">
+          <p class="mt-4 text-lg ">
             Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
             cupidatat non proident, sunt in culpa qui officia deserunt mollit
